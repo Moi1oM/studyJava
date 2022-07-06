56a49036f5d25b653d8df215d2a5c97bce27e170
Moi1ioM <hcloud0806@gmail.com>
2022-07-06 17:55:26 +0900

    2022.07.06

---
+++ src/Chapter8/Importtest.java
@@ -0,0 +1,16 @@
+package Chapter8;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Importtest {
+	public static void main(String[] args) {
+		Date today = new Date();
+		
+		SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd");
+		SimpleDateFormat time = new SimpleDateFormat("hh:mm:ss a");
+		
+		System.out.println("today's date is"+date.format(today));
+		System.out.println("time is"+time.format(today));
+	}
+}
